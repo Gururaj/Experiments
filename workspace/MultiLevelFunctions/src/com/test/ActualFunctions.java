db659fc221b4ad17d145b884479796ef7014e740
gsmayya <gsmayya@yahoo.com>
2011-03-14 08:40:48 -0700

    Trial.

---
+++ workspace/MultiLevelFunctions/src/com/test/ActualFunctions.java
@@ -0,0 +1,28 @@
+package com.test;
+
+public class ActualFunctions {
+
+	public int max(int a, int b) {
+		return (a > b) ? a: b;
+	}
+	
+	public int times(int a, int b) {
+		return a*b;
+	}
+	
+	public int min(int a,int b) {
+		return (a < b) ? a : b;
+	}
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int minus(int a,int b) {
+		return a-b;
+	}
+	
+	public int divide (int a,int b) {
+		return a/b;
+	}
+}
---
+++ workspace/MultiLevelFunctions/src/com/test/ParseFunctions.java
@@ -0,0 +1,67 @@
+
+package com.test;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Vector;
+
+
+public class ParseFunctions {
+
+	Class actualClass = null;
+	
+	Queue<Object> parsedObject = null;
+	
+	public ParseFunctions(Class<? extends Object> impl) {
+		actualClass = impl;
+	}
+	
+	// TODO: This is where the parser will come in.
+	public void parseString(String str) {
+		Vector<Object> arguments = new Vector<Object>();
+		
+		HashMap<String, String> argument = new HashMap<String,String>();
+		argument.put("10", "int");
+		arguments.add(argument);
+		HashMap<String, String> argument2 = new HashMap<String,String>();
+		argument2.put("20", "int");
+		arguments.add(argument2);
+		
+		HashMap<String,Object> row = new HashMap<String,Object>();
+		row.put("op","times");
+		row.put("args",arguments);
+		parsedObject.add(row);
+	}
+	
+	
+	public void execute() {
+		
+		Iterator<Object> itr = parsedObject.iterator();
+		while(itr.hasNext() ){
+			HashMap<String,Object> row = (HashMap<String,Object>)itr.next();
+			String function = (String) row.get("op");
+			Vector<Object> args = (Vector<Object>) row.get("args");
+			
+			if(function == "times") {
+				
+			}
+			
+			
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ParseFunctions parser = new ParseFunctions(ActualFunctions.class);
+		parser.parseString("");
+		
+		
+		
+	}
+
+}
